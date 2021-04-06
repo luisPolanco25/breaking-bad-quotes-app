88bf18acb11d0a8b5a3bb8c4a0cc642240e2370a
LuisP <luisjavierpolancomorel@gmail.com>
2021-04-05 21:15:08 -0400

    App created

---
+++ docs/precache-manifest.b75ad4b7b48413ea68f5dd4d0850a7a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4170bc2f70848d73ad60ac26a197636f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d6d22703fcc0a377f26",
+    "url": "/static/css/main.af2ac3b1.chunk.css"
+  },
+  {
+    "revision": "56a54b775bb74ae18410",
+    "url": "/static/js/2.79859a62.chunk.js"
+  },
+  {
+    "revision": "6bd4fc5180a0c8bc723c0cd6557f34e5",
+    "url": "/static/js/2.79859a62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d6d22703fcc0a377f26",
+    "url": "/static/js/main.908b2e4d.chunk.js"
+  },
+  {
+    "revision": "2de908640a893eec544c",
+    "url": "/static/js/runtime-main.c8a5a7ac.js"
+  }
+]);
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,57 @@
+import { useEffect, useRef, useState } from 'react';
+
+
+export const useFetch = (url) => {
+
+    const isMounted = useRef(true);
+
+    const [state, setState] = useState({
+        data: null,
+        loading: true,
+        error: null,
+    });
+
+    useEffect(() => {
+        return () => {
+            isMounted.current = false;
+        }
+    }, [])
+
+
+    useEffect(() => {
+
+        setState({
+            data: null,
+            loading: true,
+            error: null,
+        });
+
+        fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            
+                if (isMounted.current) {
+                    setState({
+                        loading: false,
+                        error: null,
+                        data,
+                    });
+                }
+        })
+        .catch(() => {
+            setState({
+                data: null,
+                loading: false,
+                error: 'It couldn\'t load the info'
+            })
+        });
+
+    }, [url]);
+    
+    
+    
+    return state;
+}
+
+
+
